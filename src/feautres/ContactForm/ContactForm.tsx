7e3cbf4b7aa702f6b555cb74e540e2642cb5558d
nenahui <kanatraccoon@gmail.com>
2024-07-23 13:06:47 +0600

    Added animations via framer-motion

--- src/feautres/ContactForm/ContactForm.tsx
+++ src/feautres/ContactForm/ContactForm.tsx
@@ -6,6 +6,7 @@ import type { IContact } from '../../types';
 import { fetchContactInfo } from '../Home/homeThunks';
 import { selectContactInfo, selectIsCreating, selectIsLoading } from './contactFormSlice';
 import { createContact, editContact } from './contactFormThunks';
+import { motion } from 'framer-motion';
 
 export const ContactForm = () => {
   const { id } = useParams();
@@ -57,64 +58,112 @@ export const ContactForm = () => {
   };
 
   return (
-    <Form layout={'vertical'} onFinish={onFinish} form={form}>
-      <Flex gap={'middle'} vertical>
-        <Form.Item<IContact>
-          className={'m-0'}
-          label={'Name'}
-          name={'name'}
-          rules={[{ required: true, message: 'This is a required field…' }]}
-        >
-          <Input placeholder={'Enter name…'} autoComplete={'off'} disabled={isLoading} />
-        </Form.Item>
+    <motion.div initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }}>
+      <Form layout={'vertical'} onFinish={onFinish} form={form}>
+        <Flex gap={'middle'} vertical>
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}
+            className={'mb-10'}
+          >
+            <Form.Item<IContact>
+              className={'m-0'}
+              label={'Name'}
+              name={'name'}
+              rules={[{ required: true, message: 'This is a required field…' }]}
+            >
+              <Input placeholder={'Enter name…'} autoComplete={'off'} disabled={isLoading} />
+            </Form.Item>
+          </motion.div>
 
-        <Form.Item<IContact>
-          className={'m-0'}
-          label={'Phone'}
-          name={'phone'}
-          rules={[{ required: true, message: 'This is a required field…' }]}
-        >
-          <Input placeholder={'Enter phone…'} autoComplete={'off'} />
-        </Form.Item>
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.1 }}
+            className={'mb-10'}
+          >
+            <Form.Item<IContact>
+              className={'m-0'}
+              label={'Phone'}
+              name={'phone'}
+              rules={[{ required: true, message: 'This is a required field…' }]}
+            >
+              <Input placeholder={'Enter phone…'} autoComplete={'off'} />
+            </Form.Item>
+          </motion.div>
 
-        <Form.Item<IContact>
-          className={'m-0'}
-          label={'E-Mail'}
-          name={'email'}
-          rules={[{ required: true, message: 'This is a required field…', type: 'email' }]}
-        >
-          <Input placeholder={'Enter email…'} autoComplete={'off'} disabled={isLoading} />
-        </Form.Item>
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.2 }}
+            className={'mb-10'}
+          >
+            <Form.Item<IContact>
+              className={'m-0'}
+              label={'E-Mail'}
+              name={'email'}
+              rules={[{ required: true, message: 'This is a required field…', type: 'email' }]}
+            >
+              <Input placeholder={'Enter email…'} autoComplete={'off'} disabled={isLoading} />
+            </Form.Item>
+          </motion.div>
 
-        <Form.Item<IContact>
-          className={'m-0'}
-          label={'Image'}
-          name={'image'}
-          rules={[{ required: true, message: 'This is a required field…' }]}
-        >
-          <Input
-            placeholder={'Enter image url…'}
-            autoComplete={'off'}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setImagePreview(e.target.value)}
-            disabled={isLoading}
-          />
-        </Form.Item>
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.3 }}
+            className={'mb-10'}
+          >
+            <Form.Item<IContact>
+              className={'m-0'}
+              label={'Image'}
+              name={'image'}
+              rules={[{ required: true, message: 'This is a required field…' }]}
+            >
+              <Input
+                placeholder={'Enter image url…'}
+                autoComplete={'off'}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setImagePreview(e.target.value)
+                }
+                disabled={isLoading}
+              />
+            </Form.Item>
+          </motion.div>
 
-        <Form.Item label={'Image preview'} className={'m-0'}>
-          {imagePreview.length === 0 ? (
-            <Empty description={'No image'} />
-          ) : (
-            <Image src={imagePreview} width={200} alt={'Contact image incorrect'} />
-          )}
-        </Form.Item>
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.4 }}
+            className={'mb-10'}
+          >
+            <Form.Item label={'Image preview'} className={'m-0'}>
+              {imagePreview.length === 0 ? (
+                <Empty description={'No image'} />
+              ) : (
+                <Image src={imagePreview} width={200} alt={'Contact image incorrect'} />
+              )}
+            </Form.Item>
+          </motion.div>
 
-        <Button type={'primary'} htmlType={'submit'} loading={isCreating || isLoading}>
-          Save
-        </Button>
-        <Button onClick={onCancel} danger>
-          Cancel
-        </Button>
-      </Flex>
-    </Form>
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.5 }}
+            className={'mb-10'}
+          >
+            <Flex gap={'middle'} vertical>
+              <Button type={'primary'} htmlType={'submit'} loading={isCreating || isLoading}>
+                Save
+              </Button>
+              <Button onClick={onCancel} danger>
+                Cancel
+              </Button>
+            </Flex>
+          </motion.div>
+        </Flex>
+      </Form>
+    </motion.div>
   );
 };
--- src/feautres/Home/Home.tsx
+++ src/feautres/Home/Home.tsx
@@ -1,9 +1,10 @@
-import { Flex, Spin } from 'antd';
+import { Flex, Spin, Typography } from 'antd';
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { Contact } from '../../components/Contact/Contact';
 import { selectIsContacts, selectIsFetching } from './homeSlice';
 import { fetchContacts } from './homeThunks';
+import { motion } from 'framer-motion';
 
 export const Home = () => {
   const dispatch = useAppDispatch();
@@ -20,9 +21,20 @@ export const Home = () => {
 
   return (
     <Flex gap={'middle'} vertical>
-      {contacts.map((contact) => (
-        <Contact key={contact.id} contact={contact} />
-      ))}
+      {contacts.length === 0 ? (
+        <Typography.Text className={'a-centered'}>Contact list is empty...</Typography.Text>
+      ) : (
+        contacts.map((contact, index) => (
+          <motion.div
+            key={contact.id}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: index * 0.1 }}
+          >
+            <Contact key={contact.id} contact={contact} />
+          </motion.div>
+        ))
+      )}
     </Flex>
   );
 };
